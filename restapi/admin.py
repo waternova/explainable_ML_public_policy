d94fe1729e9ad67a99da96dbebb1ee36d534a5e4
Monicah Wambugu <monicah_wambugu@berkeley.edu>
2018-03-16 00:35:10 -0700

    Modified admin view

--- restapi/admin.py
+++ restapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from restapi.models import MlModel,Factor,Comment,User
 
 # Register your models here.
+
+admin.site.register(MlModel)
+admin.site.register(Factor)
+admin.site.register(Comment)
+admin.site.register(User
