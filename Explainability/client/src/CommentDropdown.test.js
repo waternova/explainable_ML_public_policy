0a1cc21eeef96b9faee6d2bdd819437b3dae27d9
Samuel Meyer <info@samueldmeyer.com>
2018-03-27 11:16:32 -0700

    Updated CommentDropdown tests

--- Explainability/client/src/CommentDropdown.test.js
+++ Explainability/client/src/CommentDropdown.test.js
@@ -11,10 +11,22 @@ it('should render without crashing', () => {
   ReactDOM.render(<CommentDropdown />, div);
 });
 
-it('should not show a form on default render', () => {
-  const wrapper = mount(
-    <CommentDropdown />
-  );
-  const form = wrapper.find('form');
-  expect(form.length).toEqual(0);
+it('should add a new comment on handleNewComment', () => {
+  const div = document.createElement('div');
+  const commentDropdown = ReactDOM.render(<CommentDropdown />, div);
+  commentDropdown.setState({newComment: 'a comment'});
+  commentDropdown.handleNewComment(new MouseEvent('click'));
+  expect(commentDropdown.state.comments.length).toBe(1);
+  expect(commentDropdown.state.comments[0].text).toBe('a comment');
+});
+
+it('should add a two new comments on handleNewComment', () => {
+  const div = document.createElement('div');
+  const commentDropdown = ReactDOM.render(<CommentDropdown />, div);
+  commentDropdown.setState({newComment: 'a comment'});
+  commentDropdown.handleNewComment(new MouseEvent('click'));
+  commentDropdown.setState({newComment: 'another comment'});
+  commentDropdown.handleNewComment(new MouseEvent('click'));
+  expect(commentDropdown.state.comments.length).toBe(2);
+  expect(commentDropdown.state.comments[1].text).toBe('another comment');
 });
