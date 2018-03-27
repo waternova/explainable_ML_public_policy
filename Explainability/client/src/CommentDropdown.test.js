953f4de6e5411541fbf5d851ef4b18c7504ee4bd
Samuel Meyer <info@samueldmeyer.com>
2018-03-27 00:13:15 -0700

    Added first enzyme test

--- Explainability/client/src/CommentDropdown.test.js
+++ Explainability/client/src/CommentDropdown.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDropdown from './CommentDropdown.js';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 it('should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CommentDropdown />, div);
+});
+
+it('should not show a form on default render', () => {
+  const wrapper = mount(
+    <CommentDropdown />
+  );
+  const form = wrapper.find('form');
+  expect(form.length).toEqual(0);
 });
