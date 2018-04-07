4033031b5e2418c0f2b94d250c3f361d3a320c97
Samuel Meyer <info@samueldmeyer.com>
2018-04-07 10:09:13 -0700

    Removed duplicate api url so that page will load on deployed server

--- Explainability/urls.py
+++ Explainability/urls.py
@@ -24,8 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^api/', include('restapi.urls')),
-    url(r'^$', include('restapi.urls')),
-    #url(r'^$', views.model_viewer, name='model_viewer'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT
 ) + [
